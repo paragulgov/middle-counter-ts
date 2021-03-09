d3d4dd80e75ca3e9c57420fcee7e1567cf7a1dda
Ramzan Paragulgov <61330017+paragulgov@users.noreply.github.com>
2021-03-09 12:48:42 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0)
+    return (
+        <div className="App">
+            <div>{count}</div>
+            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={() => setCount(0)}>0</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
